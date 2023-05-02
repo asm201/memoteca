66f5d81d862df70b1181d1cac75818474f281437
asm201 <asirikaku2000@gmail.com>
2023-05-02 17:24:06 -0300

    criação do json server

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Eu ador muito esse grupo',
+      autoria:'Meg',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Pega essa barra de mental',
+      autoria:'Esquilo',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      autoria:'Esquilo',
+      modelo: 'modelo2'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
+  @Input() pensamento = {
+    conteudo: 'Eu amo angular',
+    autoria:'Maranhão',
+    modelo: 'modelo3'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
   }
 
 }
